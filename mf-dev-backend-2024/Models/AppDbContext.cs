65974d16ef2f673c394f812d8e997786f7728b3b
JOAO VICTOR DINIZ PIAZZA <144743654+JoaoPiazza07@users.noreply.github.com>
2024-04-24 14:47:51 -0300

    Configuração do Banco de Dados e da Tabela Veiculo

---
+++ mf-dev-backend-2024/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2024.Models
+{ // Classe resposável por fazer essa configuração do Entity Framework, para gerar a tabela/processo, 
+  // como se fosse um utilitário resposável, por pegar todas as minhas classes e gerar as tabelas dos bancos de dados
+    public class AppDbContext : DbContext //Herança
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options): base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+    }
+}
---
+++ mf-dev-backend-2024/Models/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2024.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "Obrigatório informar o nome.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa.")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o ano de fabricação .")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o ano de modelo.")]
+        public int AnoModelo { get; set; }
+
+
+        //data = configurar nosso modelo para que o nosso framework, automaticamente, use essas informações na hora de criar no banco de dados e usar nas views
+    }
+}
