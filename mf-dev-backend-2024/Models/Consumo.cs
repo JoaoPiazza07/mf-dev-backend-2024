c9cd78a5dd5f331380c641b0fc5edb6d79ab2088
JOAO VICTOR DINIZ PIAZZA <144743654+JoaoPiazza07@users.noreply.github.com>
2024-04-26 14:18:57 -0300

    Criptografia

---
+++ mf-dev-backend-2024/Models/Consumo.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2024.Models
+{
+    [Table("Consumos")]
+    public class Consumo            //consumo de veículos               
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Obrigatório informar a descrição!")]
+        [Display (Name = "Descrição")]
+        public string Descricao { get; set; } //com o o que voce consumiu
+        [Required(ErrorMessage = "Obrigatório informar a data!")]
+        public DateTime Data { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar a valor!")]
+        public decimal Valor { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar a kilometragem do veículo!")]
+        public int km{ get; set;}
+        [Required(ErrorMessage = "Obrigatório informar a descrição!")]
+        [Display(Name = "Tipo de Combustível")]
+        public TipoCombustivel Tipo {get; set;}
+        [Display(Name = "Veículo")]
+        [Required(ErrorMessage = "Obrigatório informar o veículo!")]
+        public int VeiculoId { get; set;}
+        [ForeignKey("VeiculoId")]
+        public Veiculo Veiculo { get; set;} //vitualização
+
+    }
+
+        public enum TipoCombustivel
+    {
+        Gasolina,
+        Etanol,
+        Gas,
+        Dissel 
+    }
+}
--- mf-dev-backend-2024/Models/Veiculo.cs
+++ mf-dev-backend-2024/Models/Veiculo.cs
@@ -23,6 +23,8 @@ namespace mf_dev_backend_2024.Models
         [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
 
+        public ICollection<Consumo> Consumos { get; set; }
+
 
         //data = configurar nosso modelo para que o nosso framework, automaticamente, use essas informações na hora de criar no banco de dados e usar nas views
     }
